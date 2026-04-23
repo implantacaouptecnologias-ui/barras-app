c0e114d517132f7382c6780788f83ff0b8efc59c
implantacaouptecnologias-ui <implantacaouptecnologias@gmail.com>
2026-04-23 12:44:07 -0300

    feat: adiciona favicon da empresa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logo.webp" type="image/webp" />
+        <link rel="apple-touch-icon" href="/logo.webp" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
